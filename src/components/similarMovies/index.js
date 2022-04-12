09701aa79ef23d942c12314901d32d58b11dcfe0
Abid Ali <studytimee@gmail.com>
2022-04-12 14:55:47 +0100

    Search Similar Movies: Added Component : similarMovies

---
+++ src/components/similarMovies/index.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState }  from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Link } from "react-router-dom";
+import { getSimilarMovies } from "../../api/tmdb-api";
+import { excerpt } from "../../util";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 550,
+  },
+});
+
+export default function SimilarMovies({ movie }) {
+  const classes = useStyles();
+  const [similars, setSimilars] = useState([]);
+
+  useEffect(() => {
+    getSimilarMovies(movie.id).then((similars) => {
+      setSimilars(similars);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="similar table">
+        <TableHead>
+          <TableRow>
+            <TableCell >Name</TableCell>
+            <TableCell align="center">Popular</TableCell>
+            <TableCell align="center">Overview</TableCell>
+            <TableCell align="right">More</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {similars.map((s) => (
+            <TableRow key={s.id}>
+              <TableCell component="th" scope="row">
+                {s.title}
+              </TableCell>
+              <TableCell >{excerpt(s.popularity)}</TableCell>
+              <TableCell >{excerpt(s.overview)}</TableCell>
+              <TableCell >
+                <Link
+                  to={{
+                    pathname: `/movies/${s.id}`,
+                    state: {
+                      similar: s,
+                      movie: movie,
+                    },
+                  }}
+                >
+                  Full Movie Detail
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
