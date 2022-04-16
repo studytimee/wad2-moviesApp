24a6b24de076f544a88aee97d89611b4685e0d7c
Abid Ali <studytimee@gmail.com>
2022-04-16 15:58:18 +0100

    Develop celebrities detail page component - Fetch celebrity profile images

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -143,3 +143,20 @@ export const getCelebrities = (args) => {
       throw error
     });
 };
+
+export const getCelebrityImages = ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+
+  })
+    .catch((error) => {
+      throw error
+    });
+};
