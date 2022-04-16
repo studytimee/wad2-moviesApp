60b184acb97dcefbc7eeb569f183b9ef65ea93e2
Abid Ali <studytimee@gmail.com>
2022-04-16 16:22:13 +0100

    Develop celebrities detail page component - Fetch Celebrity Profile Details
    Note:  Used parameterised routes so celebrity id is required to get celebrity details.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -144,6 +144,24 @@ export const getCelebrities = (args) => {
     });
 };
 
+//parameterised routes, celebrity id is required to fetch celebrity details.
+export const getCelebrity = (args) => {
+  const [ ,idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+    .catch((error) => {
+      throw error
+    });
+};
+
+
 export const getCelebrityImages = ({ queryKey }) => {
   const [, idPart] = queryKey;
   const { id } = idPart;
