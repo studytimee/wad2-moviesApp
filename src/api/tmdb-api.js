a3177604449ec632a8462ab6db23d598689b6e7f
Abid Ali <studytimee@gmail.com>
2022-04-16 13:35:47 +0100

    Assembling the Celebrities page - Fetch Live Celebrities Data

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -127,3 +127,19 @@ export const getNowPlayingMovies = () => {
       throw error
     });
 };
+
+export const getCelebrities = (args) => {
+  const [ pagePart] = args.queryKey;
+  const { page } = pagePart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=${page}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+    .catch((error) => {
+      throw error
+    });
+};
