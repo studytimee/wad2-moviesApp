aeb1879c5914f5b21509bbc4702e9f4727c3f010
Abid Ali <studytimee@gmail.com>
2022-04-21 09:57:09 +0100

    Integrating the Express Movie API
    SignUp : api/accounts with email, firstName, lastName and password
    
    SignIn: api/accounts/security/token with email and password.

---
+++ src/api/movie-api.js
@@ -0,0 +1,19 @@
+export const signup = (email, password, firstName, lastName) => {
+    return fetch('/api/accounts', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ email: email, password: password, firstName: firstName, lastName: lastName })
+    }).then(res => res.json())
+};
+
+export const login = (email, password) => {
+    return fetch('/api/accounts/security/token', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ email: email, password: password })
+    }).then(res => res.json())
+};
